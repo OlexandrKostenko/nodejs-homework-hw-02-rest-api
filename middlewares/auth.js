a6535b6ef65a196acbcc7e97e838ce6b285b72a9
OlexandrKostenko <106703542+OlexandrKostenko@users.noreply.github.com>
2023-03-14 11:07:25 +0200

    fix auth middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const { SECRET_KEY } = process.env;
 const auth = async(req, res, next) => {
     const { authorization = '' } = req.headers;
     const [bearer, token] = authorization.split(' ');
-    if (bearer !== 'Bearer') {
+    if (bearer !== 'Bearer' || !token) {
         res.status(401).json({
         status: "error",
         code: 401,
